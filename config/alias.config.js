676a5eff75b705258c82550c6c29b0ef37ace1f1
Radislav Erokho <radislav.erokho@artezio.com>
2021-02-09 01:38:52 +0300

    update webpack config

---
+++ config/alias.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  __service: path.resolve(__dirname, '../src/service'),
+  __controllers: path.resolve(__dirname, '../src/controllers'),
+  __routes: path.resolve(__dirname, '../src/routes'),
+  __constants: path.resolve(__dirname, '../src/constants'),
+  __models: path.resolve(__dirname, '../src/models'),
+  __modules: path.resolve(__dirname, '../src/modules'),
+  __utils: path.resolve(__dirname, '../src/utils'),
+  __types: path.resolve(__dirname, '../src/types'),
+};
--- config/webpack.default.config.js
+++ config/webpack.default.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
+
+const entry = require('./entries.config');
+const alias = require('./alias.config');
 
 const webpackDefaultConfig = {
-  entry: {
-    app: './src/index.ts',
-    containerDealer: './src/service/ContainerDealer/index.ts',
-  },
+  entry,
 
   target: 'node',
   output: {
     filename: '[name].js',
     path: __dirname + './../dist',
+    libraryTarget: 'umd',
   },
 
   module: {
@@ -22,22 +22,13 @@ const webpackDefaultConfig = {
       {
         test: /\.ts$/,
         loader: 'ts-loader',
-        exclude: [/node_modules/],
+        exclude: [/node_modules/, /dist/],
       },
     ],
   },
 
   resolve: {
-    alias: {
-      __service: path.resolve(__dirname, '../src/service'),
-      __controllers: path.resolve(__dirname, '../src/controllers'),
-      __routes: path.resolve(__dirname, '../src/routes'),
-      __constants: path.resolve(__dirname, '../src/constants'),
-      __models: path.resolve(__dirname, '../src/models'),
-      __modules: path.resolve(__dirname, '../src/modules'),
-      __utils: path.resolve(__dirname, '../src/utils'),
-      __types: path.resolve(__dirname, '../src/types'),
-    },
+    alias,
     extensions: ['.ts', '.js'],
   },
 };
